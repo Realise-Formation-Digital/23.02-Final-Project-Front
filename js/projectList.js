191f94f2d389dab6372139d6fcc7d52be3aff8c3
Oshidori99 <126858336+Oshidori99@users.noreply.github.com>
2023-06-05 17:13:25 +0200

    Affichage de la liste des projets partie 1

--- js/projectList.js
+++ js/projectList.js
@@ -13,13 +13,17 @@ import { API_BASE_URL } from "../constants/constants.js";
  */
 async function getProjects() {
     try {
-        const response = await axios.get(API_BASE_URL + 'projects')
+        const response = await axios.get(API_BASE_URL + 'projects');
         const projectList = response.data;
-        console.log("sdfgerhe");
+        const projectListText = response.data.toString('utf-8'); // Conversion en texte avec encodage UTF-8
+        // Utilisez projectListText pour travailler avec les données texte si nécessaire
         console.log(response);
-        let totalProjects = response.data.length;
+        console.log(projectList);
+        
+        // let totalProjects = response.data.length;
         let projectListEl = document.getElementById("projectList");
         projectListEl.innerHTML = "";
+
 
         // add pagination elements
         //paginationEl();
@@ -27,13 +31,23 @@ async function getProjects() {
 
         for (let project of projectList) {
             const block = document.createElement("li");
-            block.setAttribute('id', project.id)
+            block.setAttribute('id', project.id);
             block.classList.add("list-group-item");
-            block.classList.add('list-group-item-action')
+            block.classList.add('list-group-item-action');
             projectListEl.appendChild(block);
-
+          
             const h3 = document.createElement("h3");
             h3.innerText = project.title;
+            block.appendChild(h3);
+          
+            for (let copil of project.copil_list) {
+              const copilBlock = document.createElement("div");
+              const copilH5 = document.createElement("h5");
+              copilH5.innerText = copil.first_name  + " " + copil.last_name ;
+              copilBlock.appendChild(copilH5);
+              block.appendChild(copilBlock);
+            }
+          
 
             // const img = document.createElement("img");
             // img.src = project.image_url;
@@ -44,7 +58,7 @@ async function getProjects() {
 
             // a.appendChild(img);
             // block.appendChild(a);
-            block.appendChild(h3);
+            //block.appendChild(h3);
         }
     } catch (e) {
         throw e;
