012ec7ce3ba912e7cfbb596092dfcffe982d5b8f
Fellec <b_fallek@yahoo.fr>
2023-06-08 12:56:50 +0200

    last commit

--- js/projectList.js
+++ js/projectList.js
@@ -1,207 +1,116 @@
 import { API_BASE_URL } from "../constants/constants.js";
-import { projectListener } from "./projects.js";
-
-let totalBeers = 25;
-let page = 1;
-let perPage = 12;
-let beerFilter = "";
 
 /**
- * Get beers
+ * Get Projects
  *
  * @returns {Promise<void>}
  */
-async function getBeers() {
-    try {
-        const response = await axios.get(API_BASE_URL + `beers?page=${page}&per_page=${perPage}&beer_filter=${beerFilter}`);
-        const beerList = response.data.data;
-        totalBeers = response.data.metadata.total;
-        let beerListEl = document.getElementById("beerList");
-        beerListEl.innerHTML = "";
+async function getProjects() {
+  let response = null
 
-        // add pagination elements
-        paginationEl();
-        paginationBeersListener();
+  try {
+    response = await axios.get(API_BASE_URL + "projects");
+  } catch (err) {
+    console.log(err)
+    throw err
+  }
+  const projectList = response.data;
+  const projectListText = response.data.toString("utf-8"); // Conversion en texte avec encodage UTF-8
+  // Utilisez projectListText pour travailler avec les données texte si nécessaire
 
-        for (let beer of beerList) {
+  // let totalProjects = response.data.length;
+  let projectListEl = document.getElementById("projectList");
 
-            //create update button
-            const buttonUpdate = document.createElement("button");
-            buttonUpdate.classList.add("btn");
-            buttonUpdate.classList.add("btn-warning");
-            buttonUpdate.classList.add("float-end");
-            buttonUpdate.classList.add("me-1");
-            buttonUpdate.classList.add("update-button");
-            buttonUpdate.innerText = "Modifier";
+  for (let project of projectList) {
+    // Elements creation
+    const liElement = document.createElement("li");
+    const contentBlockElement = document.createElement("div");
+    const contentBlockElementTitle = document.createElement("div");
+    const contentBlockElementSpan = document.createElement('span')
+    const iconElement = document.createElement('i')
+    let listCopil = ""
 
-            //create input hidden for update button
-            const inputUpdate = document.createElement("input");
-            inputUpdate.type = "hidden";
-            inputUpdate.name = "id";
-            inputUpdate.value = beer.id
+    // Custom LI Element
+    liElement.setAttribute("id", project.id); // set attribute ID
+    liElement.classList.add("list-group-item", "list-group-item-action", "d-flex", "justify-content-between", "align-items-start");
 
-            //create form update button
-            const buttonUpdateForm = document.createElement("form");
-            buttonUpdateForm.action = `./addProject.html?id=${project.id}`;
-            buttonUpdateForm.appendChild(inputUpdate);
-            buttonUpdateForm.appendChild(buttonUpdate);
+    for (let i = 0; i < project.copil_list.length; i++) {
+      // if (i === project.copil_list.length -1 ) {
+      //   listCopil = listCopil.concat(project.copil_list[i].first_name + " " + project.copil_list[i].last_name)
+      // }
+      // else{
+      //   listCopil = listCopil.concat(project.copil_list[i].first_name + " " + project.copil_list[i].last_name + ", ")
+      // } 
 
-            //create delete button
-            const buttonDelete = document.createElement("button");
-            buttonDelete.classList.add("btn");
-            buttonDelete.classList.add("btn-danger");
-            buttonDelete.classList.add("float-end");
-            buttonDelete.classList.add("delete-button");
-            buttonDelete.addEventListener('click', async function(){
-                const url = API_BASE_URL + 'beers?id=' + beer.id
-               console.log(url)
-               await axios({
-                method: 'delete',
-                url: url})
-                getBeers();
-            })
-            buttonDelete.innerText = "Supprimer";
+      i === project.copil_list.length - 1 ? listCopil = listCopil.concat(project.copil_list[i].first_name + " " + project.copil_list[i].last_name) : listCopil = listCopil.concat(project.copil_list[i].first_name + " " + project.copil_list[i].last_name + ", ")
+    }
 
-            //create detail button
-            const buttonDetail = document.createElement("button");
-            buttonDetail.classList.add("btn");
-            buttonDetail.classList.add("btn-primary");
-            buttonDetail.classList.add("float-start");
-            buttonDetail.classList.add("detail-button");
-            buttonDetail.innerText = "Détail";
+    // Custom Content Block
+    contentBlockElement.classList.add("ms-2", "me-auto");
+    contentBlockElement.innerHTML = `
+      <div class="fw-bold">${project.title}</div>
+      ${listCopil}`
 
-            //create card-footer
-            const cardFooter = document.createElement("div");
-            cardFooter.classList.add("card-footer");
-            cardFooter.appendChild(buttonDetail);
-            cardFooter.appendChild(buttonDelete);
-            cardFooter.appendChild(buttonUpdateForm);
+    // Custom span element
+    contentBlockElementSpan.classList.add("badge", "bg-primary", "rounded-pill");
 
-            // create brewers-tips
-            const brewersTips = document.createElement("li");
-            brewersTips.classList.add("list-group-item");
-            brewersTips.innerText = beer.brewers_tips;
+    // Custom icon element
+    iconElement.classList.add("bi", "bi-trash-fill", "danger")
 
-            // create description
-            const description = document.createElement("li");
-            description.classList.add("list-group-item");
-            description.innerText = beer.description;
+    // Attach Icon -> Span
+    contentBlockElementSpan.appendChild(iconElement)
 
-            // create tagline
-            const tagline = document.createElement("li");
-            tagline.classList.add("list-group-item");
-            tagline.classList.add("fw-bold");
-            tagline.innerText = beer.tagline;
+    // Attach Title --> Content
+    contentBlockElement.appendChild(contentBlockElementTitle)
 
-            //create ul
-            const ul = document.createElement("ul");
-            ul.classList.add("list-group");
-            ul.classList.add("list-group-flush")
-            ul.appendChild(tagline);
-            ul.appendChild(brewersTips);
-            ul.appendChild(description);
+    // Add event Listener to SPAN
+    contentBlockElementSpan.addEventListener('click', async function (evt) {
+      evt.stopPropagation()
+      let responseDelete = null
+      let idProjectDelete = evt.target.parentElement.parentElement.getAttribute('id')
+      try {
+        //envoyer la requette au serveur via l'URL et l'id du Kanban
+        responseDelete = await axios.delete(API_BASE_URL + "projects/" + idProjectDelete);
+      } catch (err) {
+        throw err
+      }
 
-            // create title
-            const title = document.createElement("h5");
-            title.classList.add("card-title");
-            title.innerText = beer.name;
-
-            //create image
-            const image = document.createElement("img");
-            image.src = beer.image_url;
-            image.classList.add("mx-auto");
-            image.classList.add("mt-3");
-
-            //create card-body
-            const cardBody = document.createElement("div");
-            cardBody.classList.add("card-body");
-            cardBody.appendChild(title);
-            cardBody.appendChild(ul);
-
-            //create card
-            const card = document.createElement("div");
-            card.classList.add("card")
-            card.classList.add("h-100");
-            card.appendChild(image);
-            card.appendChild(cardBody);
-            card.appendChild(cardFooter);
-
-            //create element
-            const element = document.createElement("div");
-            element.classList.add("col");
-            element.classList.add("mb-3");
-            element.id = beer.id;
-            element.appendChild(card);
-
-            beerListEl.appendChild(element);
-        }
-
-        beersListener();
+      if (responseDelete.status === 200) {
+        console.log(responseDelete.status)
+        alert("Votre projet a été retiré de la liste")
+        setTimeout(location.reload(),1000);
+ 
         
-    } catch (e) {
-        throw e;
-    }
-}
+      } else {
+          alert("Quelque chose s'est mal passé")
+      }
 
-/**
- * Pagination create elements
- */
-function paginationEl() {
-    let nbPage;
-    if (totalBeers % perPage === 0) {
-        nbPage = totalBeers / perPage;
-    } else {
-        nbPage = Math.floor(totalBeers / perPage) + 1;
-    }
+      
 
-    let ulPagination = document.getElementById("pagination");
-    ulPagination.innerHTML = "";
-    //create buttons pages
-    for (let pageNumber = 1; pageNumber <= nbPage; pageNumber++) {
-        const aPageEl = document.createElement("a");
-        aPageEl.href = "#";
-        aPageEl.classList.add("page-link");
-        aPageEl.value = pageNumber;
-        aPageEl.innerHTML = pageNumber.toString();
+    })
 
-        const liPage = document.createElement("li");
-        liPage.classList.add("page-item");
-        if (pageNumber === page) {
-            liPage.classList.add("active");
-        }
-        liPage.appendChild(aPageEl);
 
-        ulPagination.appendChild(liPage);
-    }
-}
 
-/**
- * Search beers listener
- */
-let searchBeers = document.getElementById("searchBeers");
-searchBeers.addEventListener("keyup", async (e)  => {
-    try {
-        beerFilter = e.target.value;
-        await getBeers();
-    } catch(e) {
-        throw e;
-    }
-})
+    // Final Step --> Attach Block and span to li element
+    liElement.appendChild(contentBlockElement)
+    liElement.appendChild(contentBlockElementSpan)
 
-/**
- * Pagination beers listener
- */
-const paginationBeersListener = function() {
-    document.querySelectorAll('li.page-item').forEach(beerPageLi => {
-        beerPageLi.addEventListener('click', async (e) => {
-            try {
-                page = e.target.value;
-                await getBeers();
-            } catch(e) {
-                throw e;
-            }
-        });
+    //Add event listener to LI
+    liElement.addEventListener("click", function (evt) {
+      let kanban_id = evt.target.parentElement.parentElement.getAttribute('id')
+      if (!kanban_id) kanban_id = evt.target.getAttribute('id')
+
+      window.open(
+        "http://127.0.0.1:5500/Kanban.html?kanban_id=" +
+        kanban_id,
+        "_blank"
+      );
     });
+
+    // Final FINAL Step --> Attach li to ul
+    projectListEl.appendChild(liElement);
+  }
+
 }
 
-await getBeers();
+await getProjects();
