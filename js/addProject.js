bf6f972cbe90ac70fb49958c9aa5504fbdd4d9e5
Fellec <b_fallek@yahoo.fr>
2023-06-08 12:35:05 +0200

    recupération id checkbox - checked :)

--- js/addProject.js
+++ js/addProject.js
@@ -1,42 +1,62 @@
 import { API_BASE_URL } from "../constants/constants.js";
-
-let usersGlobalList = null
-// datepicker with months instead days
-// $("#creationDate").datepicker( {
-//     format: "dd/mm/yyyy",
-//     viewMode: "months",
-//     minViewMode: "months"
-// });
-
+//Création de la liste des utilisateurs et initialisation vide 
+let usersGlobalList = null;
+/**
+ * Création de la function vérifier pour venir recup le statut de nos check box
+ * 
+ */
+function verfierUser() {
+  let checkedUser = [];
+  const checked = document.querySelectorAll(".daniel");
+  for (let i of checked) {
+    if (i.checked === true) {
+      checkedUser.push(i.id);
+    }
+  }
+}
+//création de la function 
 async function getUsers() {
   try {
     const response = await axios.get(API_BASE_URL + "users");
     const usersList = response.data;
-    //console.log(users)
-    //return users
-    //const usersHTML = users.map((user) => `<li>${user}</li>`).join("");
+    const ulElement = document.getElementById("lista");
+    let idTabUser = [];
+    console.log(idTabUser);
 
-    //const dropdownMenu = document.querySelector(".dropdown-menu");
-    //dropdownMenu.innerHTML = usersHTML;
+    for (let user of usersList) {
+      const liCheck = document.createElement("li");
+      const optionEl = document.createElement("label");
+      const checkBox = document.createElement("input");
 
-    const selectElement = document.getElementById('select-users')
-    for(let user of usersList){
-        const optionEl = document.createElement('option')
-        optionEl.setAttribute('value', user.id)
-        optionEl.innerText = user.first_name + " " + user.last_name
-        selectElement.appendChild(optionEl)
+      //perso li
+      liCheck.classList.add("list-group-item");
+
+      //perso input
+      checkBox.classList.add("form-check-input", "me-1", "mx-3");
+      checkBox.classList.add("daniel");
+      checkBox.setAttribute("type", "checkbox");
+      checkBox.setAttribute("id", user.id);
+
+      //perso label
+      optionEl.classList.add("form-check-label", "stretched-link");
+      optionEl.setAttribute("for", user.id);
+      optionEl.innerText = user.first_name + " " + user.last_name;
+      idTabUser.push(user.id);
+      liCheck.appendChild(checkBox);
+      liCheck.appendChild(optionEl);
+      ulElement.appendChild(liCheck);
     }
-
-
-
-
+    //recupération du btn
+    const btnValidation = document.getElementById("valid");
+    btnValidation.addEventListener("click", verfierUser);
+    console.log(btnValidation);
   } catch (e) {
     console.error(e);
     throw e;
   }
 }
 
-await getUsers()
+await getUsers();
 
 async function createProject() {
   try {
@@ -64,49 +84,12 @@ async function createProject() {
   }
 }
 
-//
+
+//Ajouter un listener sur le bouton de l'ajout d'un projet
 document.getElementById("newProject").addEventListener("click", () => {
   const myModal = new bootstrap.Modal(
     document.getElementById("new-project-modal"),
     {}
   );
   myModal.show();
-
-  // createProject().then(project => {
-
-  //         //remove error message
-  //         const errorElement = document.getElementById("errorMessage");
-  //         errorElement.innerHTML = '';
-
-  //         //add success message
-  //         const successMessageEl = document.createElement("div");
-  //         successMessageEl.innerHTML = "Le projet a bien été ajouté.";
-  //         successMessageEl.classList.add("alert");
-  //         successMessageEl.classList.add("alert-success");
-  //         successMessageEl.innerText = `Le projet ${project.title} a bien été ajoutée.`;
-  //         const successElement = document.getElementById("successMessage");
-  //         successElement.innerHTML = '';
-  //         successElement.appendChild(successMessageEl);
-
-  //         //empty inputs when success
-  //         const form = document.getElementById("newProject");
-  //         Array.from(form.elements).forEach((element) => {
-  //             element.value = "";
-  //         });
-  //     }
-  // ).catch(e => {
-  //         //remove success message
-  //         const successElement = document.getElementById("successMessage");
-  //         successElement.innerHTML = "";
-
-  //         //add error message
-  //         const error = document.createElement("div");
-  //         error.classList.add("alert");
-  //         error.classList.add("alert-danger");
-  //         error.innerText = e.response.data.message;
-  //         const errorElement = document.getElementById("errorMessage");
-  //         errorElement.innerHTML = '';
-  //         errorElement.appendChild(error);
-  //     }
-  // );
 });
